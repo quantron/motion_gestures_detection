89aac9362faa2a5d4b5e8ae1510b5f1beeee0bf5
Ivanov Evgeniy <evgeny.ivanov@quantron-systems.com>
2018-12-01 14:58:11 +0300

    experiments

--- GesturesManager/app/src/main/java/uk/co/lemberg/motion_gestures/activity/TestActivity.java
+++ GesturesManager/app/src/main/java/uk/co/lemberg/motion_gestures/activity/TestActivity.java
@@ -1,15 +1,18 @@
 package uk.co.lemberg.motion_gestures.activity;
 
 import android.graphics.Color;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Looper;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,18 +36,27 @@ import uk.co.lemberg.motion_gestures.utils.Utils;
 
 public class TestActivity extends AppCompatActivity {
 
+	public enum GestureType {
+		MoveLeft,
+		MoveRight
+	}
+
+	public interface Listener {
+		void onGestureRecognized(GestureType gestureType);
+	}
+
 	private static final String TAG = TestActivity.class.getSimpleName();
 
 	private static final int GESTURE_DURATION_MS = 1280000; // 1.28 sec
 	private static final int GESTURE_SAMPLES = 128;
 
 	// region tensorflow
-	private static final String MODEL_FILENAME = "file:///android_asset/frozen_optimized_quant.pb";
+	private static final String MODEL_FILENAME = "file:///android_asset/frozen_optimized_quant_r.pb";
 
 	private static final String INPUT_NODE = "x_input";
 	private static final String OUTPUT_NODE = "labels_output";
 	private static final String[] OUTPUT_NODES = new String[]{OUTPUT_NODE};
-	private static final int NUM_CHANNELS = 2;
+	private static final int NUM_CHANNELS = 3;
 	private static final long[] INPUT_SIZE = {1, GESTURE_SAMPLES, NUM_CHANNELS};
 	private static final String[] labels = new String[]{"Right", "Left"};
 
@@ -56,6 +68,16 @@ public class TestActivity extends AppCompatActivity {
 	private final float[] recognData = new float[GESTURE_SAMPLES * NUM_CHANNELS];
 	private final float[] filteredData = new float[GESTURE_SAMPLES * NUM_CHANNELS];
 	private int dataPos = 0;
+
+
+	private static final float RISE_THRESHOLD = 0.99f;
+	private static final float FALL_THRESHOLD = 0.9f;
+	private static final long MIN_GESTURE_TIME_MS = 400000; // 0.4 sec - the minimum duration of recognized positive signal to be treated as a gesture
+	//private static final long GESTURES_DELAY_TIME_MS = 1000000; // 1.0 sec - minimum delay between two gestures
+
+	private long gestureStartTime = -1;
+	private GestureType gestureType = null;
+	private boolean gestureRecognized = false;
 
 	private TensorFlowInferenceInterface inferenceInterface;
 
@@ -72,6 +94,13 @@ public class TestActivity extends AppCompatActivity {
 
 	private boolean recStarted = false;
 	private long firstTimestamp = -1;
+
+//	public TestActivity(Context context, Listener listener) {
+//		this.context = context;
+//		this.listener = listener;
+//		mainHandler = new Handler(Looper.getMainLooper());
+//	}
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -187,7 +216,7 @@ public class TestActivity extends AppCompatActivity {
 
 			recordingData[dataPos++] = event.values[0] / DATA_NORMALIZATION_COEF;
 			recordingData[dataPos++] = event.values[1] / DATA_NORMALIZATION_COEF;
-			//recordingData[dataPos++] = event.values[2] / DATA_NORMALIZATION_COEF;
+			recordingData[dataPos++] = event.values[2] / DATA_NORMALIZATION_COEF;
 			if (dataPos >= recordingData.length) {
 				dataPos = 0;
 			}
@@ -205,17 +234,19 @@ public class TestActivity extends AppCompatActivity {
 			inferenceInterface.fetch(OUTPUT_NODE, outputScores);
 			long stopTime = SystemClock.elapsedRealtimeNanos();
 
-			final float x = filteredData[filteredData.length - 2];
-			final float y = filteredData[filteredData.length - 1];
+			final float x = filteredData[filteredData.length - 3];
+			final float y = filteredData[filteredData.length - 2];
+            final float z = filteredData[filteredData.length - 1];
+
 			final long runTime = stopTime - startTime;
 			final float leftProbability = outputScores[0];
 			final float rightProbability = outputScores[1];
-
+            Log.i(TAG,"DRAG:" + (leftProbability));
 			// update UI
 			runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
-					updateUI(runTime, floatTimestampMicros, x, y, leftProbability, rightProbability);
+					updateUI(runTime, floatTimestampMicros, x, y, z, leftProbability, rightProbability);
 				}
 			});
 		}
@@ -231,25 +262,26 @@ public class TestActivity extends AppCompatActivity {
 				if (i - j * NUM_CHANNELS < 0) continue;
 				output[i + 0] += input[i + 0 - j * NUM_CHANNELS] * ir;
 				output[i + 1] += input[i + 1 - j * NUM_CHANNELS] * ir;
-				//output[i + 2] += input[i + 2 - j * NUM_CHANNELS] * ir;
+				output[i + 2] += input[i + 2 - j * NUM_CHANNELS] * ir;
 			}
 		}
 	}
 
-	private void updateUI(long runTime, float timestampMicros, float x, float y, float leftProbability, float rightProbability) {
+	private void updateUI(long runTime, float timestampMicros, float x, float y,  float z, float leftProbability, float rightProbability) {
 		txtProcessTime.setText("Time: " + Utils.formatNsDuration(runTime));
 
-		leftProbability -= 0.75; // -0.75..0.25
-		leftProbability *= 4; // -3..1
-		if (leftProbability < -1) leftProbability = -1;
-		rightProbability -= 0.75; // -0.25..0.25
-		rightProbability *= 4; // -3..1
-		if (rightProbability < -1) rightProbability = -1;
+		leftProbability -= 0.50; // -0.75..0.25
+		leftProbability *= 2; // -3..1
+		if (leftProbability < 0) leftProbability = 0;
+		rightProbability -= 0.50; // -0.25..0.25
+		rightProbability *= 2; // -3..1
+		if (rightProbability < 0) rightProbability = 0;
 
 		float probabilityTimestamp = timestampMicros - GESTURE_DURATION_MS / 1000 / 2;
 
 		addPoint(X_INDEX, timestampMicros, x);
 		addPoint(Y_INDEX, timestampMicros, y);
+        addPoint(Z_INDEX, timestampMicros, z);
 
 		if (probabilityTimestamp > 0) {
 			addPoint(RIGHT_INDEX, probabilityTimestamp, rightProbability);
@@ -258,21 +290,67 @@ public class TestActivity extends AppCompatActivity {
 
 		chart.notifyDataSetChanged();
 		chart.invalidate();
+
+		detectTestGestures(leftProbability, rightProbability);
 	}
 
+	private void detectTestGestures(float leftProb, float rightProb) {
+		if (gestureStartTime == -1) {
+			// not recognized yet
+			if (getHighestProb(leftProb, rightProb) >= RISE_THRESHOLD) {
+				gestureStartTime = SystemClock.elapsedRealtimeNanos();
+				gestureType = getGestureType(leftProb, rightProb);
+			}
+		}
+		else {
+			GestureType currType = getGestureType(leftProb, rightProb);
+			if ((currType != gestureType) || (getHighestProb(leftProb, rightProb) < FALL_THRESHOLD)) {
+				// reset
+				gestureStartTime = -1;
+				gestureType = null;
+				gestureRecognized = false;
+			}
+			else {
+				// gesture continues
+				if (!gestureRecognized) {
+					long gestureTimeMs = (SystemClock.elapsedRealtimeNanos() - gestureStartTime) / 1000;
+					if (gestureTimeMs > MIN_GESTURE_TIME_MS) {
+						gestureRecognized = true;
+						callListener(gestureType);
+					}
+				}
+			}
+		}
+	}
+
+
 	// region chart helper methods
-	private static final String[] LINE_DESCRIPTIONS = {"X", "Y", "Right", "Left"};
-	private static final int[] LINE_COLORS = {0x70FF0000, 0x7000FF00, 0x70FFFF00, 0x7000FFFF};
+	private static final String[] LINE_DESCRIPTIONS = {"X", "Y", "Z", "Barrel", "Drag"};
+	private static final int[] LINE_COLORS = {0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFF00, 0xFF00FFFF};
 
 	private static final int X_INDEX = 0;
 	private static final int Y_INDEX = 1;
-	private static final int RIGHT_INDEX = 2;
-	private static final int LEFT_INDEX = 3;
+    private static final int Z_INDEX = 2;
+	private static final int RIGHT_INDEX = 3;
+	private static final int LEFT_INDEX = 4;
 
 	private void createDataSets() {
 		for (int i = 0; i < LINE_DESCRIPTIONS.length; i++) {
 			chart.getLineData().addDataSet(createLineDataSet(LINE_DESCRIPTIONS[i], LINE_COLORS[i]));
 		}
+	}
+
+
+	private void callListener(final GestureType gestureType) {
+		Log.i(TAG, "GESTURE RECOGNIZED:" + (gestureType));
+	}
+
+	private static float getHighestProb(float leftProb, float rightProb) {
+		return Math.max(leftProb, rightProb);
+	}
+
+	private static GestureType getGestureType(float leftProb, float rightProb) {
+		return (leftProb > RISE_THRESHOLD) ? GestureType.MoveLeft : GestureType.MoveRight;
 	}
 
 	private void addPoint(int dataSetIndex, float x, float y) {
